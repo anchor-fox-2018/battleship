4729f9bf72e2a94d6777ee385f506f8f36e21f39
days88 <yayahidayatullah@rocketmail.com>
2018-09-08 17:50:09 +0800

    belum jadi

--- battleship.js
+++ battleship.js
@@ -1,1 +1,29 @@
 // Your code here
+const kolom = 10;
+const baris = 10;
+function boardBuild(kolom,baris){
+    const abc = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    //let hasil = []
+    for(let i = 0;i < kolom;i++){
+        let isi = []; 
+        for(let j = 0;j <= baris;j++){
+            if(i === 0){
+                if(j === 0){
+                    isi.push(' ');
+                }else{
+                    isi.push(j)
+                }
+            }else{
+                if(j === 0){
+                    isi.push(abc[i]);
+                }else{
+                    isi.push(' ')
+                }
+            }
+        }
+        console.log(isi)
+        
+    }
+    return hasil.join('')
+}
+boardBuild(kolom,baris)
